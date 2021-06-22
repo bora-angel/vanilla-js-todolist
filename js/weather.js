00e7d864cb290f2ccacd4c9cf8d03ec6bbf72744
bora-angel <qhfkkqhfk@gmail.com>
2021-06-23 00:30:27 +0900

    Add files via upload

--- js/weather.js
+++ js/weather.js
@@ -4,9 +4,7 @@ function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url)
-    .then( response => response.json())
-    .then((data)=>{
+    fetch(url).then( response => response.json()).then((data)=>{
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
         city.innerText = data.name;

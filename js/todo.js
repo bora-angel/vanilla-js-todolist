0846912787de255f50ef4b444854d5b65ad2a200
bora-angel <qhfkkqhfk@gmail.com>
2021-06-23 00:25:37 +0900

    Add files via upload
    노마드 코더 인강 바닐라 JS로 크롬 앱 만들기

---
+++ js/todo.js
@@ -0,0 +1,54 @@
+const toDoForm =  document.getElementById("todo-form");
+// document.querySelector("#todo-form input")와 같음;
+const toDoInput = toDoForm.querySelector("input"); 
+const toDoList =  document.getElementById("todo-list");
+let toDos = [];
+const TODOS_KEY =  "todos";
+
+//toDos의 내용을 localStorage에 넣어주는 함수.
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+    toDos = toDos.filter((toDo)=> toDo.id !== parseInt(li.id));
+    saveToDos();
+}
+
+function paintToDo(newToDo){
+    const li = document.createElement("li");
+    li.id = newToDo.id;
+    const span = document.createElement("span");
+    span.innerText = newToDo.text;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    const newTodoObj = {
+        text:newTodo,
+        id:Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
